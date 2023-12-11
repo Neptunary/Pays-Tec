fbffa0414eb989c5f5c5a658e9fd9a69325d56a8
Neptunary <106664418+Neptunary@users.noreply.github.com>
2023-12-11 15:17:49 +0100

    Update App.js

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,52 +1,82 @@
-import React, { useState, useEffect } from 'react';
 
-function PaysComponent() {
-  const [pays, setPays] = useState('');
-  const [informationPays, setInformationPays] = useState(null);
+import './App.css';
+import { useEffect, useState } from 'react';
 
-  const afficherInformation = () => {
-    alert("Vous avez tapé : " + pays);
+function App() {
+  const [pays, setPays] = useState([]);
+
+  const fetchPays = () => {
+    fetch('http://localhost:3000/pays', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPays(data); // Utilisez setPays pour mettre à jour l'état avec les données récupérées
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des données :', error);
+      });
+  };
+
+  const addPays = () => {
+    fetch('http://localhost:3000/pays', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        pays: 'Nouveau Pays',
+        ville: 'Nouvelle Ville'
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        fetchPays();
+      })
+      .catch((error) => {
+        console.error('Erreur lors de l\'ajout du pays :', error);
+      });
+  };
+
+  const deletePays = (id) => {
+    fetch(`http://localhost:3000/pays/${id}`, {
+      method: 'DELETE'
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        fetchPays();
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la suppression du pays :', error);
+      });
   };
 
   useEffect(() => {
-    if (pays.trim() !== '') {
-      fetch(`https://restcountries.com/v3.1/name/${pays}`)
-        .then(response => response.json())
-        .then(data => {
-          if (data.length > 0) {
-            setInformationPays(data[0]);
-          } else {
-            alert("Pays non trouvé. Veuillez vérifier l'orthographe et réessayer.");
-          }
-        })
-        .catch(error => console.error('Erreur lors de la récupération des informations du pays', error));
-    } else {
-      setInformationPays(null);
-    }
-  }, [pays]);
+    fetchPays();
+  }, []);
 
   return (
-    <div>
-      <label htmlFor="paysInput">Tapez un pays :</label>
-      <input type="text" id="paysInput" placeholder="Entrez un pays" value={pays} onChange={(e) => setPays(e.target.value)} />
+    <div className="App">
+      Affichage de Villes importants et surs à visiter
+      {pays.map((pays) => (
+        <div key={pays.id}>
+          <h2>{pays.nom}</h2>
 
-      <button onClick={afficherInformation}>Information</button>
-
-      {informationPays && (
-        <div>
-          <h2>Informations sur le pays</h2>
-          <p>Capitale: {informationPays.capital}</p>
-          <p>Population: {informationPays.population}</p>
-          <p>Continent: {informationPays.region}</p>
-          <p>Villes à visiter: {/* Ajoutez ici une liste de villes à visiter en fonction des données disponibles */}</p>
-          {/* Ajoutez d'autres informations selon les propriétés disponibles dans les données du pays */}
         </div>
-      )}
+      ))}
+      <button onClick={addPays}>Ajouter un pays</button>
+      <button onClick={() => deletePays(1)}>Supprimer un pays</button>
     </div>
   );
 }
 
-export default PaysComponent;
+export default App;
 
 
 
